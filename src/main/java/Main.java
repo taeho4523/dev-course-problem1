b3bd2b46a7452a134e0f04c0e4f54f8ecddfdcde
Taeho KIM <laferrari963@naver.com>
2025-03-18 17:46:40 +0900

    테스트용 메인

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,16 @@
+import command.CommandProcessor;
+
+import console.InputClass;
+import console.OutputClass;
+import post.PostManager;
+
 public class Main {
+    public static void main(String[] args) {
+        InputClass input = new InputClass();
+        OutputClass output = new OutputClass();
+        PostManager postManager = new PostManager();
+
+        CommandProcessor processor = new CommandProcessor(input, output, postManager);
+        processor.run();
+    }
 }
